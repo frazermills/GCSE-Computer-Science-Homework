2c8d39f47a42b49416f2e0a7345e96498d7c0fed
frazermills <77229450+frazermills@users.noreply.github.com>
2021-03-05 11:52:10 +0000

    Update Simple-Calculator.py

--- Practicle-2/Simple-Calculator.py
+++ Practicle-2/Simple-Calculator.py
@@ -1,1 +1,43 @@
+# Author: Frazer Mills
+# Date: 05/03/21
+# program name: 'Simple-Calculator.py'
+# Python 3.9.2
+# Description: This program is a simple calculator that takes two numbers and performs an operation chosen by the user.
 
+# ------------------------------------ Initialising variables ------------------------------------ #
+num1 = 0
+num2 = 0
+result = 0
+operator = None
+menu = """
+Which operator would you like:
+1. Subtract
+2. Multiply
+3. Divide
+4. Add
+Input operator: """
+
+# --------------------------------------- Takes user input --------------------------------------- #
+num1 = int(input("Input number 1: "))
+num2 = int(input("Input number 2: "))
+
+operator = int(input(menu))
+
+# -------------------------------- Selects which calculation to do ------------------------------- #
+if operator == 1:
+    result = num1 - num2
+    
+elif operator == 2:
+    result = num1 * num2
+    
+elif operator == 3:
+    result = num1 / num2
+    
+elif operator == 4:
+    result = num1 + num2
+
+else:
+    print("That was an illegal input, please try again")
+
+# ----------------------------------- Outputs result to console ---------------------------------- #
+print(result if result else "No result caculated")

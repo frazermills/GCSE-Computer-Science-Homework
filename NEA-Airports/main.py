c00ad0942d215c47c40c35108e3ebdd432a2c5f6
frazermills <77229450+frazermills@users.noreply.github.com>
2021-04-01 18:56:09 +0100

    Added command 1 of 5

--- NEA-Airports/main.py
+++ NEA-Airports/main.py
@@ -3,42 +3,96 @@ import csv
 
 
 def main():
-    airport_data = read_airports_data()
-    print(airport_data)
-    menu = menu_system.Menu()
-    menu_handler(menu)
+    formatted_airport_data = {
+        "UK Airports": {
+            "LPL": "Liverpool John Lennon",
+            "BOH": "Bournemouth International"
+        },
+        "Overseas Airports": None
+    }
 
+    aircraft_types = {
+        "Medium narrow body": {
+            "Running cost per seat per 100km": 8,
+            "Maximun flight range": 2650,
+            "Capacity": 180,
+            "Minimum number of first class seats": 8
+        },
+        
+        "Large narrow body": {
+            "Running cost per seat per 100km": 7,
+            "Maximun flight range": 5600,
+            "Capacity": 220,
+            "Minimum number of first class seats": 10
+        },
+        "Medium wide body": {
+            "Running cost per seat per 100km": 5,
+            "Maximun flight range": 4050,
+            "Capacity": 406,
+            "Minimum number of first class seats": 14
+        }
+    }
+
+    airport_data = read_airports_data()
+    formatted_airport_data["Overseas Airports"] = process_airports_data(airport_data)
+
+    distance = formatted_airport_data["Overseas Airports"]
+    
+    menu = menu_system.Menu(formatted_airport_data, aircraft_types)
+    menu_handler(menu, formatted_airport_data)
 
 def read_airports_data():
-    data = []
-
     with open("Airports.txt") as f:
         reader = csv.reader(f, delimiter=",")
-        for row in reader:
-            if len(row):
-                print(row)
-                data.append(row)
+        data = [
+            row for row in reader
+            if len(row)
+        ]
+
     return data
 
+def process_airports_data(data):
+    formatted_airport_data = {
+        
+        airport_data[0]: {
+            "Airport name": airport_data[1],
+            "Distance from Liverpool John Lennon": airport_data[2],
+            "Distance from Bournemouth Interational": airport_data[3]
+        }
+        for i, airport_data in enumerate(data)
+    }
 
-def menu_handler(menu):
+    return formatted_airport_data
+
+
+def menu_handler(menu, airport_data):
     in_menu = True
 
     while in_menu:
-      while menu.option == None:
-          menu.setup_menu()
-          if menu.choice == 1:
-              menu.menu_options()
-          elif menu.choice == 2:
-              menu.menu_options()
-          elif menu.choice == 3:
-              menu.menu_options()
-          elif menu.choice == 4:
-              menu.menu_options()
-          elif menu.choice == 9:
-              menu.menu_options()
-          
-          menu.update_menu()
+    
+        while menu.option == None:
+            menu.setup_menu()
+            menu.get_menu_options()
+
+            if menu.option == menu.commands[0]:
+                uk_airport_code, overseas_airport_code = menu.enter_airport_details()
+                uk_airport_details = airport_data["UK Airports"][uk_airport_code]
+                overseas_airport_details = airport_data["Overseas Airports"][overseas_airport_code]
+
+            elif menu.option == menu.commands[1]:
+                menu.enter_flight_details()
+            elif menu.option == menu.commands[2]:
+                print("Enter price plan")
+            elif menu.option == menu.commands[3]:
+                print("Clear data")
+            elif menu.option == menu.commands[4]:
+                in_menu = False
+                menu.quit_menu()
+            else:
+                print("Illegal input, please try again")
+
+        menu.option = None
+
             
 if __name__ == "__main__":
     main()
--- NEA-Airports/menu_system.py
+++ NEA-Airports/menu_system.py
@@ -1,48 +1,88 @@
-
 class Menu:
-  def __init__(self):
-    self.commands = ["1 - Enter airport details", "2 - Enter flight details", "3 - Enter price plan", "4 - Clear data", "9 - Quit"]
-    self.choice = None
-    self.option = None
+    def __init__(self, airport_data, aircraft_types):
+        self.commands = ["1 - Enter airport details", "2 - Enter flight details", "3 - Enter price plan", "4 - Clear data", "9 - Quit"]
+        self.choice = None
+        self.option = None
+        self.airport_data = airport_data
+        self.valid_codes = {
+            "UK Airports": [*self.airport_data["UK Airports"]],
+            "Overseas Airports": [*self.airport_data["Overseas Airports"]]
+        }
+        self.aircraft_types = aircraft_types
+        self.developer = "Frazer Mills"
+        self.company = "Airport Manager LLC"
 
-  def setup_menu(self):
-    for index, command in enumerate(self.commands):
-      print(command)
-    self.choice = int(input("menu choice >>> "))
-  
-  def formatting(self, mode):
-    if mode == "clear_display":
-      for i in range(60):
-        print("\n")
+    def setup_menu(self):
+        for index, command in enumerate(self.commands):
+            print(command)
+        self.choice = int(input("menu choice >>> "))
 
-    elif mode == "print_horizontal_rule":
-      print("=" * 60)
+    def formatting(self, mode):
+        if mode == "clear_display":
+            for i in range(60):
+                print("\n")
 
-    elif mode == "clear_data":
-      print("data cleared")
-  
-  def menu_options(self):
-    if self.choice == 1:
-      self.option = self.commands[0]
+        elif mode == "print_horizontal_rule":
+            print("=" * 60)
 
-    elif self.choice == 2:
-      self.option = self.commands[1]
+        elif mode == "clear_data":
+            print("data cleared")
 
-    elif self.choice == 3:
-      self.option = self.commands[2]
+    def get_menu_options(self):
+        if self.choice == 1:
+            self.option = self.commands[0]
 
-    elif self.choice == 4:
-      self.option = self.commands[3]
-  
-  def update_menu(self):
-    print(self.option)
-    if self.option == self.commands[0]:
-      print("1 - Enter airport details")
-    elif self.option == self.commands[1]:
-      print("2 - Enter flight details")
-    elif self.option == self.commands[2]:
-      print("3 - Enter price plan")
-    elif self.option == self.commands[3]:
-      print("4 - Clear data")
-    elif self.option == self.commands[4]:
-      print("9 - Quit")
+        elif self.choice == 2:
+            self.option = self.commands[1]
+
+        elif self.choice == 3:
+            self.option = self.commands[2]
+
+        elif self.choice == 4:
+            self.option = self.commands[3]
+
+        elif self.choice == 9:
+            self.option = self.commands[4]
+
+    def update_menu(self):
+        print(self.option)
+        if self.option == self.commands[0]:
+            self.enter_airport_details()
+        elif self.option == self.commands[1]:
+            print("2 - Enter flight details")
+        elif self.option == self.commands[2]:
+            print("3 - Enter price plan")
+        elif self.option == self.commands[3]:
+            print("4 - Clear data")
+        elif self.option == self.commands[4]:
+            print("9 - Quit")
+
+    def enter_airport_details(self):
+        three_letter_uk_airport_code = str(input("Input three letter UK Airport code >>> "))
+        while three_letter_uk_airport_code not in self.valid_codes["UK Airports"]:
+            print("Invalid UK Airport code, please try again")
+            three_letter_uk_airport_code = str(input("Input three letter UK Airport code >>> "))
+
+        three_letter_overseas_airport_code = str(input("Input three letter Overseas Airport code >>> "))
+        while three_letter_overseas_airport_code not in self.valid_codes["Overseas Airports"]:
+            print("Invalid Overseas Airport code, please try again")
+            three_letter_overseas_airport_code = str(input("Input three letter Overseas Airport code >>> "))
+
+        chosen_airport = self.airport_data["Overseas Airports"][three_letter_overseas_airport_code]["Airport name"]
+        print(f"You have selected to fly to {chosen_airport} Airport")
+
+        return three_letter_uk_airport_code, three_letter_overseas_airport_code
+
+    def enter_flight_details(self):
+        pass
+
+    def enter_price_plan(self):
+        pass
+
+    def clear_data(self):
+        pass
+
+    def quit_menu(self):
+        print(f"Thank you for using this program.\nThis program was created by {self.developer} from {self.company}")
+        quit()
+
